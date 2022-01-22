8d5f72713c01a7d2ba375fdab15d6e1bf144ef3e
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-01-22 11:43:40 -0500

    commititng the initial html

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+var quizQuestion = [
+    {
+    title: "question title",
+    answerOne: "one",
+    answerTwo: "two",
+    answerThree: "three",
+    answerFour: "four",
+    correct: "four"
+},
+]
+
+var indexNumber = 0
+
+//create eventListener based on answer div
+
+console.log(quizQuestion[indexNumber].title)
+quizQuestion[2].title
